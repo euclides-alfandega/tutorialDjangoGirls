fef01037806551fdeb65d62167b064a46495c68b
nully.alfandega <euclidesnealfandega@gmail.com>
2020-09-11 12:33:30 +0200

    Primeiro commit

--- blog/models.py
+++ blog/models.py
@@ -10,10 +10,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     creates_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    publishe_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.publishe_date = timezone.now()
         self.save()
     
     def __str__(self):
