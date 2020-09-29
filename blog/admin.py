28a3e53765f159098c13d022ef0a8f3c1440d44a
nully.alfandega <euclidesnealfandega@gmail.com>
2020-09-29 16:42:21 +0200

    actualizacao das mudancas feitas no blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+   
+   list_display = ('author', 'title', 'text', 'publishe_date')
 
-admin.site.register(Post)
+   search_fields = ['author']
+    
+admin.site.register(Post, PostAdmin)
